d7d27d9b76d82c6fefe596ea6d9529a93d12c2af
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2025-02-19 10:46:33 +0100

    Update Traffic light for blind people.c

--- Traffic light for blind people.c
+++ Traffic light for blind people.c
@@ -1,12 +1,20 @@
+/*
+  Titel: Blindenampel für Fußgänger mit Taste
+  Verwendete Hardware: Funrduino 2560, LEDs, Taste, Wiederstände 200 und 10k Ohm, USB-Kabel, Buzzer
+  Autor: kisalu-id
+  Datum: 19. Februar 2025
+  Lizenz: MIT-Lizenz
+*/
+
 //declare LEDs
 int led_red = 9;
 int led_yellow = 10;
 int led_green = 11;
-int led_peasant_green = 3;
-int led_peasant_red = 4;
+int led_peasant_green = 8;
+int led_peasant_red = 7;
 
-//declare button
-int btn = 5;
+//declare button und buzzer
+int btn = 6;
 int buzzer = 12;
 
 //declare timing
@@ -19,79 +27,70 @@ void setup() {
   pinMode(led_green, OUTPUT);
   pinMode(led_peasant_green, OUTPUT);
   pinMode(led_peasant_red, OUTPUT);
-
-  //button is as input
-  pinMode(btn, INPUT);
-  //buzzer as output
-  pinMode(buzzer, OUTPUT);
   
-  //turn LEDs off
+  //turn them off
   digitalWrite(led_red, LOW);
   digitalWrite(led_yellow, LOW);
   digitalWrite(led_green, LOW);
   digitalWrite(led_peasant_green, LOW);
   digitalWrite(led_peasant_red, LOW);
   
+  //button is as input
+  pinMode(btn, INPUT);
+  //buzzer as output
+  pinMode(buzzer, OUTPUT);
   
   //test
   Serial.begin(9600);
-
 }
 
 /*a function that turns on red light each 40 seconds automatically, so other cars can cross the road in fromt of you;
 and if a pedestrian will press the button, the red light also turns on
 */
-void btnPress() {
-
-  for (int i=0; i<40; i++) {
-    //declare a variable state just for convenience
-    int state = digitalRead(btn); 
-    
-    if (state == 0) {
-      Serial.println(i);
-      delay(1000); //countdown 1 sec * 40 times, for other cars to cross the road
-    }
-    
-    else if (state == 1) {
-      Serial.println("The button was pressed!");
-      delay(delayTime);
-      return;
-    }
-  }
-}
-
-void loop() {
-  //start with green light being on for 40 sec
-  digitalWrite(led_green, HIGH);
-  digitalWrite(led_peasant_red, HIGH);
-  Serial.println("Green!");
-  btnPress();
+void btnPressed() {
   digitalWrite(led_green, LOW);
-
-  //if the button is pressed OR if 40 sec passed by:
   digitalWrite(led_yellow, HIGH);
   digitalWrite(led_peasant_red, HIGH);
-  Serial.println("Yellow!");
+  Serial.println("Yellow for cars!");
   delay(delayTime/2);
   digitalWrite(led_yellow, LOW);
   
   digitalWrite(led_red, HIGH);
   digitalWrite(led_peasant_red, LOW);
   digitalWrite(led_peasant_green, HIGH);
-  for (int i=0; 10>=i; i++) {
-    tone(12,1000); //sound with frequency 1000Hz
-    delay(100); //100ms
-    noTone(12); //turn off the buzzer
-    delay(100);
+  
+  for (int i=0; 10>=i; i++)b{
+    tone(12,200); //Erzeuge Ton an Pin 12 mit der Frequenz 1000Hz
+    delay(100); //100ms warten
+    noTone(12); //Schalte Ton aus
+    delay(100); //100ms warten
   }
-  Serial.println("Red!");
+
+  Serial.println("Red for cars!");
   delay(delayTime);
   digitalWrite(led_red, LOW);
-  
+
   digitalWrite(led_yellow, HIGH);
   digitalWrite(led_peasant_red, HIGH);
   digitalWrite(led_peasant_green, LOW);
-  Serial.println("Yellow!");
+  Serial.println("Yellow for cars!");
   delay(delayTime/2);
   digitalWrite(led_yellow, LOW);
+}
+
+void loop() {
+  //start with green light being on for cars
+  digitalWrite(led_green, HIGH);
+  digitalWrite(led_peasant_red, HIGH);
+  Serial.println("Green for cars!"); //endless loop, hehe
+
+  //declare a variable state just for convenience
+  int state = digitalRead(btn);
+
+  //if the button is pressed, start the function
+  if (state == 1) {
+    Serial.println("The button was pressed!");
+    delay(delayTime);
+    btnPressed();
+  }
 }
