cd21f9943dbba7fd8da489a8e87c3720d46203e2
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2025-03-14 09:15:24 +0100

    Update Optical Data Transmission.c

--- Optical Data Transmission.c
+++ Optical Data Transmission.c
@@ -1,47 +1,97 @@
-//TMP36
+/*
+Titel: Optische Datenübertragung mit Arduino  
+  Beschreibung: Dieses Programm misst die Temperatur mit einem TMP36-Sensor,  
+  wandelt die Spannung in Celsius um und konvertiert die Temperatur in eine  
+  Binärzahl. Diese Binärdaten werden dann über eine Laserdiode gesendet.  
+  Ein zweiter Arduino empfängt die Daten mit einem Fotodetektor und rekonstruiert  
+  die Temperatur.  
+
+  Ablauf:  
+  1. Temperatur mit TMP36-Sensor messen  
+  2. Analogwert in Spannung (mV) umrechnen  
+  3. Spannung in Temperatur (°C) umwandeln  
+  4. Temperaturwert in Binärformat konvertieren  
+  5. Binärdaten über Laserdiode senden  
+  6. Empfänger-Arduino empfängt und dekodiert die Daten  
+
+  Verwendete Hardware:  
+  - Arduino Mega 2560 (Sender & Empfänger)  
+  - TMP36 Temperatursensor  
+  - Laserdiode  
+  - Lichtwellenleiter (LWL)  
+  - Fotodetektor  
+  - USB-Kabel 
+  
+  Lizenz: MIT-Lizenz
+*/
+
+#include <math.h>
+
 int sensorPin = A0;
+int laserPin = 9;
     
 void setup() {
-    Serial.begin(9600);
+  pinMode(laserPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-	int tempInput = analogRead(sensorPin);
+  //read temperature sensor
+  int tempInput = analogRead(sensorPin);
   float tempInVolt = tempInput * 5.0; //or * 3.3
   tempInVolt /= 1024.0;
 
+  //print voltage
   Serial.print("Voltage = ");
-  Serial.print(tempInVolt); //print temperature
+  Serial.print(tempInVolt);
   Serial.print(" mV;\t"); 
-	
-  float temperatureC = (voltage - 0.5) * 100;
-  Serial.print("Temperature = ");
+
+  //print temperature
+  float temperatureC = (tempInVolt - 0.5) * 100;
+  Serial.print("temperatureRounded = "); 
   Serial.print(temperatureC);
   Serial.println(" °C");
 
-  int tempBinary = translateToBinary(temperatureC);
-	
-  delay(2000);
+  //round up to int
+  int temperatureRounded = ceil(temperatureC);
+  int temperatureCtest = 29;
+
+  //convert to binary
+  String binaryString = translateToBinary(temperatureRounded);
+  Serial.println();
+  Serial.print("Temperature in binary = ");
+  Serial.print(binaryString);
+  Serial.println();
+
+  //send binary via laser
+  sendBinary(binaryString);
+
+  //delay(10000);
 }
 
-int translateToBinary(num) {
-  int binaryNum[32]; //assuming 32 bit integer.
-  int i=0;
-
+String translateToBinary(int num) {
   if (num == 0) {
     Serial.println("0");
-    return 0;
+    return "0";
   }
 
-   while (num > 0) {
-      binaryNum[i++] = num % 2;
-      num /= 2;
-   }
+  String binaryString = "";
+  while (num > 0) {
+    binaryString = String(num % 2) + binaryString;
+    num /= 2;
+  }
+  return binaryString;
+}
 
-  //print in reverse
-
-  //clear from unnecesary 0's
-
-
-  return answer;
+void sendBinary(String binaryString) {
+  for (int i = 0; i < binaryString.length(); i++) {
+    //ternary conditional operator (my favourite shortcut UwU)
+    analogWrite(laserPin, binaryString[i] == '1' ? 225 : 128);
+    delay(500);
+    analogWrite(laserPin, 0);
+    delay(500);
+  }
+  //turn it off for 1 second after sending
+  analogWrite(laserPin, 0);
+  delay(1000);
 }
