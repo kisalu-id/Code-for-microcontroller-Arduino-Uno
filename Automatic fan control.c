1e8bc16fc60ab0bc0a49ae9c45ee555294144ece
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2025-03-26 16:58:26 +0100

    Update Automatic fan control.c
    Added a solution for random temperature measurments and negative distance measurments

--- Automatic fan control.c
+++ Automatic fan control.c
@@ -1,12 +1,24 @@
 /*
+DE:
  Titel: Automatische Lüftersteuerung für Serverräume
  Beschreibung: Dieses Projekt misst die Temperatur eines Serverraums mit einem TMP36-Sensor
  und steuert einen Lüfter über PWM. Ein Ultraschallsensor erkennt Personen in der Nähe
  und schaltet den Lüfter ab, um Energie zu sparen und Lärm zu reduzieren.
- Verwendete Hardware: Arduino Mega 2560, TMP36 Temperatursensor, 5V PWM-gesteuerter Lüfter, 
+ Verwendete Hardware: Arduino Mega 2560, TMP36 Temperatursensor, 12V PWM-gesteuerter Lüfter, 
  HC-SR04 Ultraschall-Abstandssensor, 16x2 LCD mit I2C, Breadboard, Kabel
 */
 
+/*
+ENG:
+ Title: Automatic Fan Control for Server Rooms
+ Description: This project measures the temperature of a server room using a TMP36 sensor
+ and controls a fan via PWM. An ultrasonic sensor detects people nearby
+ and turns off the fan to save energy and reduce noise.
+ Used Hardware: Arduino Mega 2560, TMP36 Temperature Sensor, 12V PWM-controlled Fan,
+ HC-SR04 Ultrasonic Distance Sensor, 16x2 LCD with I2C, Breadboard, Wires
+*/
+
+ 
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C LCD(0x27,16,2);
 
@@ -17,9 +29,10 @@ int SEND_PIN = 3;      //send an ultrasonic pulse from the sensor to measure dis
 int ECHO_PIN = 4;      //receives the reflected ultrasonic pulse
 
 //variables for storing measurement values
-double temperature = 0;
+float temperature = 0;
+float lastValidTemperature;
 int duration;
-long distance;
+int distance;
 int fanSpeed = 0;
 
 void setup() {
@@ -29,6 +42,7 @@ void setup() {
   
   LCD.init();   //initialize the lcd, turn on the light
   LCD.backlight();
+
   //initialize serial communication at 9600 baud
   //Baud rate - the speed at which data is transmitted or received over a communication channel, such as serial communication.
   //It represents the number of signal changes (or symbols) per second.
@@ -37,47 +51,126 @@ void setup() {
 }
 
 void loop() {
-  temperature = getTemperature();
+  //for the first iteration, get the first valid measurment 
+  if (temperature == 0) {
+    lastValidTemperature = temperature = calibrateTemperature(); //calibrate and store first temperature
+  }
+
+  //for following iterations
+  else {
+    //get the next measurment
+    temperature = getTemperature();
+
+    //check if the next measurment is significantly different than the previous one
+    //with the help of the abs() function, which gets the absolute value
+    if (abs(temperature - lastValidTemperature) > 5.0) {
+      //if it's a large change, calibrate again and store the new valid temperature
+      lastValidTemperature = temperature = calibrateTemperature();
+    }
+  }
+
   distance = getDistance();
   
   //fan speed settings
-  if (distance < 1) {    //1 meter
+  if (distance == 404) {   //if these's an error in reading the distance, turn off the fan (just in case)
+    fanSpeed = 0; 
+  } else if (distance < 1) {  //1 cm - so the testing is easier
     fanSpeed = 0;   //turns off the fan if a someone is nearby
   } else if (temperature < 20) {
-    fanSpeed = 0; //turns off the fan at very low T
+    fanSpeed = 0;   //turns off the fan at very low T
   } else if (temperature >= 20 && temperature < 25) {
-    fanSpeed = 80; //low speed for medium T
+    fanSpeed = 80;  //low speed for medium T
   } else if (temperature >= 25 && temperature < 30) {
     fanSpeed = 160; //medium speed for warmer T
   } else {
-    fanSpeed = 255; //maximum speed at high T
+    fanSpeed = 255; //maximum speed at a very high T
   }
+
   analogWrite(FAN_PWM_PIN, fanSpeed);
 
-  LCD.clear();
-  LCD.setCursor(0, 0); //start to print at the first row & first line
+  LCD.clear();  //clear the display - TODO: check, if that's really necessary
+  LCD.setCursor(0, 0);  //start to print at the first row & first line
   LCD.print(temperature);
   LCD.print(" C;");
+
   LCD.setCursor(0, 1); //print at the first row & second line
-  LCD.print(distance);
-  LCD.print(" cm");
+  if (distance != 404) {
+    //if the distance is valid (not 404), display it
+    LCD.print(distance);
+    LCD.print(" cm");
+  } else {
+    //if the distance is 404 (invalid), display "Error" on the LCD screen
+    LCD.print("Error");
+  }
 
   //serial monitor print
   Serial.print(temperature);
   Serial.print(" C;   ");
-  Serial.print(distance);
-  Serial.print(" cm");
+  if (distance != 404) {
+    //if the distance is valid (not 404), display it
+    Serial.print(distance);
+    Serial.print(" cm");
+  } else {
+    //f the distance is 404 (invalid), display "Error" on the serial monitor
+    Serial.print("Error");
+  }
+
   Serial.println();
 
-  delay(1000);
+  delay(988); //in function getDistance() we wait 12 ms; the program has to output values each second
+}
+
+//there's a problem, that sometimes we get temperature fluctuations, which are not realistic for typical room temperatures: 
+//29.18 C; 49.22 C; 41.89 C; 60.95 C; 30.65 C; 8.16 C; 20.38 C;
+//the solution to that is sorting several measurments and taking one from the middle of the array
+float calibrateTemperature() {
+  //get 10 temperature measurments
+  Serial.print("Calibrating temperature: ");
+  //n = number of mesurments, should be minimum 7
+  int n = 10;
+  float arrayTemp[n];
+  for (int i=0; i<n; i++) {
+    arrayTemp[i] = getTemperature();
+    Serial.print(arrayTemp[i]);
+    Serial.print(";  ");
+  }
+  Serial.println();
+
+  //bubble sort
+  bubbleSort(n, arrayTemp);
+
+  //take the one in the middle as the correct one
+  //both odd and even numbers for n will work
+  int nMid = n / 2;
+  Serial.print(nMid + 1);
+  Serial.print(" is the measurment number from the middle of the array, the value is: ");
+  Serial.print(arrayTemp[nMid]);
+  Serial.println();
+
+  return arrayTemp[nMid];
+}
+
+//easy but effective sorting algorithm
+int bubbleSort(int n, float arrayTemp[]) {
+  float temp;
+  for (int i=0; i<n-1; i++) {
+    for (int j=0; j<n-i-1; j++) {
+      if (arrayTemp[j] > arrayTemp[j+1]) {
+        //swap elements, if they're in wrong order
+        temp = arrayTemp[j];
+        arrayTemp[j] = arrayTemp[j+1]; 
+        arrayTemp[j+1] = temp;
+      }
+    }
+  }    //the array is being modified in place, so there's no need to return it
 }
 
 //function to measure temperature with the TMP36 sensor
 //reads the analog input and calculates the temperature in C
 float getTemperature() {
   int sensorValue = analogRead(TEMP_SENSOR);
-  float voltage = sensorValue * (5.0 / 1024.0); //to V
-  return (voltage - 0.5) * 100.0; //to T, Celsius
+  float voltage = sensorValue * (5.0 / 1024.0);  //to V, Volt
+  return (voltage - 0.5) * 100.0;    //to T, Celsius
 }
 
 //measure distance with the HC-SR04 ultrasonic sensor, returns he distance
@@ -90,5 +183,13 @@ int getDistance() {
 
     digitalWrite(SEND_PIN, LOW);   //turn off
     duration = pulseIn(ECHO_PIN, HIGH);
-    return duration * 0.034 / 2;; // conversion to cm
+
+    //convert to distance (in cm)
+    int dist = duration * 0.034 / 2;
+
+    //filter out unrealistic readings (e.g., negative or overly large distances)
+    if (dist < 0 || dist > 300) {
+      dist = 404;  //error code XD
+    }
+    return dist;
 }
