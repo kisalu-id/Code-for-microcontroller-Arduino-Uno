50e332fbfb94b58c8601518723b6f197141624b7
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2025-06-20 14:50:58 +0200

    Update Conference room.c

--- Conference room.c
+++ Conference room.c
@@ -3,26 +3,23 @@ LiquidCrystal_I2C LCD(0x27,16,2); //16 columns, 2 rows
 
 #include <Servo.h>    
 Servo jalousieServo;  
-//Servo BeamerwandServo;  //TODO: ?
+Servo beamerwandServo;
 
 //pin declarations
-const int SERVO_PIN = 6
+const int LED_PIN = 3;            //LED
+const int LIGHT_SENSOR = A2;      //LDR or light sensor ???
+const int DIMMER_SENSOR = A0;      //TODO: implement
 
-const int LED_PIN = 9             //LED
-const int LIGHT_SENSOR = A0       //LDR or light sensor ???
-const int DIMMER_SENSOR = 9;      //TODO: implement
+const int BTN_PIN = 2;            //btn to start the meeting 
 
-const int BTN_PIN = 5;            //btn to start the meeting 
+const int TEMP_SENSOR = A1;       //temperature sensor
+const int FAN_PWM_PIN = 11;        //PWM output for the fan to cool down the room if it's too hot
 
-const int TEMP_SENSOR = A0;       //temperature sensor
-const int FAN_PWM_PIN = 9;        //PWM output for the fan
+const int SEND_PIN_DOOR = 9;      //send an ultrasonic pulse from the sensor to measure distance
+const int ECHO_PIN_DOOR = 10;      //receives the reflected ultrasonic pulse - if shorter than set up, the door was opened or someone is in the room
 
-const int SEND_PIN_DOOR = 3;      //send an ultrasonic pulse from the sensor to measure distance
-const int ECHO_PIN_DOOR = 4;      //receives the reflected ultrasonic pulse - if shorter than set up, the door was opened or someone is in the room
-
-const int SEND_PIN_HOOMAN = 3;    //send an ultrasonic pulse from the sensor to measure distance to the wall
-
-const int POT_PIN = A10;          //potentiometer
+const int SEND_PIN_HOOMAN = 12;    //send an ultrasonic pulse from the sensor to measure distance to the wall
+const int ECHO_PIN_HOOMAN = 13;
 
 //variables for storing measurement values
 bool turnOn = true; //global toggle for turn on/off the "meeting mode" (with Beamerwand, etc.)
@@ -41,6 +38,30 @@ const int distanceForOpenedDoor = 3;
 
 
 
+//easy but effective sorting algorithm
+int bubbleSort(int n, float arrayTemp[]) {
+  float temp;
+  for (int i=0; i<n-1; i++) {
+    for (int j=0; j<n-i-1; j++) {
+      if (arrayTemp[j] > arrayTemp[j+1]) {
+        //swap elements, if they're in wrong order
+        temp = arrayTemp[j];
+        arrayTemp[j] = arrayTemp[j+1];
+        arrayTemp[j+1] = temp;
+      }
+    }
+  }    //the array is being modified in place, so there's no need to return it
+}
+
+
+//function to measure temperature with the TMP36 sensor
+//reads the analog input and calculates the temperature in C
+float getTemperature() {
+  //int sensorValue = analogRead(TEMP_SENSOR);
+  //float voltage = sensorValue * (5.0 / 1024.0);  //to V, Volt
+  //return (voltage - 0.5) * 100.0;    //to T, Celsius
+  return analogRead(TEMP_SENSOR) * 0.48828125 - 50.0; //AI; combining constants to reduce runtime math
+}
 
 
 void jalousieMove(bool turnOn) {
